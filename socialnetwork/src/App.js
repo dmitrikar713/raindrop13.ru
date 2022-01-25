1fe2d98708e2757dcbf37d51dee731d33efbac5d
dmitrikar713 <dmitrikar713@gmail.com>
2022-01-26 02:00:07 +0300

    dodelay uje etot cors

--- socialnetwork/src/App.js
+++ socialnetwork/src/App.js
@@ -3,16 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hero from './Components/Hero/Hero';
 import Content from './Components/Content/Content';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
       <div className='mainLayout'>
         <Hero />
         <Content />
       </div>
-    </BrowserRouter>
   );
 }
 
